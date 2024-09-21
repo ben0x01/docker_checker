e5795f4251d34a0d1d2dac961e8ea7a4ddf48782
ben0x01 <122745733+ben0x01@users.noreply.github.com>
2024-09-21 19:21:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import config
 
 from server_connecter import SshConnector
-from helper import DOCKER_CHECKER_PATH, PYTHON_INSTALLER_PATH, DOCKER_RESTARTER_PATH, SETUP_CRONTAB_PATH
+from helper import DOCKER_CHECKER_PATH, PYTHON_INSTALLER_PATH, DOCKER_RESTARTER_PATH, SETUP_CRONTAB_PATH, \
+    RIVALZ_RESTARTER, SCREEN_CHECKER
 
 if __name__ == '__main__':
     ssh = SshConnector(host=config.host, login=config.username, password=config.password)
@@ -14,6 +15,8 @@ if __name__ == '__main__':
     ssh.send_files_give_permission(DOCKER_CHECKER_PATH, "/root/modules/docker_checker.py")
     ssh.send_files_give_permission(DOCKER_RESTARTER_PATH, "/root/modules/docker_restarter.sh")
     ssh.send_files_give_permission(SETUP_CRONTAB_PATH, "/root/modules/setup_crontab.sh")
+    ssh.send_files_give_permission(RIVALZ_RESTARTER, "/root/modules/rivalz_restarter.sh")
+    ssh.send_files_give_permission(SCREEN_CHECKER, "/root/modules/screen_checker.py")
 
     ssh.execute_command("/root/modules/py_installer.sh")
     ssh.execute_command("/root/modules/setup_crontab.sh")
