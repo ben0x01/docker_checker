bffbc523ab7f31b08db807636f5c2f4bf7b99c0d
ben0x01 <122745733+ben0x01@users.noreply.github.com>
2024-09-21 20:27:24 +0500

    Update helper.py

--- helper.py
+++ helper.py
@@ -5,3 +5,6 @@ DOCKER_CHECKER_PATH = os.path.join(BASE_DIR, "data_to_send", "docker_checker.py"
 DOCKER_RESTARTER_PATH = os.path.join(BASE_DIR, "data_to_send", "docker_restarter.sh")
 PYTHON_INSTALLER_PATH = os.path.join(BASE_DIR, "data_to_send", "py_installer.sh")
 SETUP_CRONTAB_PATH = os.path.join(BASE_DIR, "data_to_send", "setup_crontab.sh")
+RIVALZ_RESTARTER = os.path.join(BASE_DIR, "data_to_send", "rivalz_restarter.sh")
+SCREEN_CHECKER = os.path.join(BASE_DIR, "data_to_send", "screen_checker.py")
+OMNI_INSTALLER_AND_RESTARTER = os.path.join(BASE_DIR, "data_to_send", "omni.sh")
